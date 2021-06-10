c644ecbd001b38714b7a5444e085ae95bdb821ab
jimin.joo <wnwlals22@naver.com>
2021-06-10 18:29:30 +0900

    create Hello world job

---
+++ src/main/java/com/example/batch/HelloWorldTasklet.java
@@ -0,0 +1,17 @@
+package com.example.batch;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+
+public class HelloWorldTasklet implements Tasklet {
+
+	@Override
+	public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+		System.out.println("작업 시작");
+
+		System.out.println("작업 종료");
+		return null;
+	}
+}
